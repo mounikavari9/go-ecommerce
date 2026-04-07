52a4213d7a1ccd52107e1aa6a15b91a326e5eb7c
Mounika <mounikavari9@gmail.com.com>
2026-04-06 23:40:51 -0500

    feat: add user routes for auth and product APIs

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"github.com/mounikavari9/go-ecommerce/controllers"
+	"github.com/gin-gonic/gin"
+
+)
+
+func UserRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.POST("/users/signup", controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.Login())
+	incomingRoutes.POST("/users/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
+	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery())
+}
