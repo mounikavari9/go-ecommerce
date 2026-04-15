ecc433a30467a9676592147a2fc12f23fcb3dfbf
Mounika <mounikavari9@gmail.com.com>
2026-04-15 14:23:23 -0500

    feat: implement auth and product controllers with validation, hashing and search functionality

---
+++ controllers/controllers.go
@@ -0,0 +1,264 @@
+package controllers
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/mounikavari9/go-ecommerce/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var UserCollection *mongo.Collection = database.UserData(database.Client, "Users") 
+var ProductCollection *mongo.Collection = database.ProductData(database.Client, "Products") 
+var Validate = validator.New()
+
+
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err!= nil{
+		log.Panic(err)
+	}
+	return string(bytes)
+}
+
+
+func VerifyPassword(userPassword string, givenPassword string) (bool, string){
+	err := bcrypt.CompareHashAndPassword([]byte(givenPassword), []byte(userPassword))
+	valid := true 
+	msg := ""
+
+	if err!= nil{
+		msg = "Login or Password is incorrect"
+		valid = false 
+	}
+	return valid, msg 
+
+}
+
+
+
+func Signup() gin.HandlerFunc{
+
+	return func(c *gin.Context){
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var user models.User 
+		if err := c.BindJSON(&user); err!= nil{
+			c.JSON{http.StatusBadRequest, gin.H{"error": err.Error()}}
+			return 
+		}
+
+		validationErr := Validate.Struct(user)
+		if validationErr != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+			return 
+		}
+
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		if err!= nil{
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return 
+		}
+		if count >0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "user already exists"})
+		}
+
+
+		count, err = UserCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		defer cancel()
+		if err!= nil{
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return 
+		}
+		if count >0{
+			c.JSON(http.StatusBadRequest, gin.H{"error": "this phone no is already in use"})
+			return 
+		}
+
+		password := HashPassword(*user.Password)
+		user.Password = &password 
+
+		user.Created_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Updated_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.User_ID = user.ID.Hex()
+
+
+		token, refresh_token, _ := generate.TokenGenerator(*user.Email, *user.First_Name, *user.Last_Name, user.User_ID)
+		user.Token = &token
+		user.Refresh_Token = &refresh_token
+
+		user.UserCart = make([]models.ProductUser, 0)
+		user.Address_Details = make([]models.Address, 0)
+		user.Order_Status = make([]models.Order, 0)
+
+		//insert in user collection database
+		_, inserterr := UserCollection.InsertOne(ctx, user)
+		if inserterr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "the user did not get created"})
+			return 
+		}
+		defer cancel()
+
+		//Response body
+		c.JSON(http.StatusCreated, "Successfully signed in!")
+
+	}
+
+}
+
+
+
+func Login() gin.HandlerFunc{
+	return func(c *gin.Context){
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		//request body
+		var user models.User 
+		if err := c.BindJSON(&user); err!= nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return 
+		}
+
+		//check user exists in db
+		err := UserCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&founduser)
+		defer cancel()
+
+		if err!= nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "login or password incorrect"})
+			return 
+		}
+
+		//verify user password 
+
+		PasswordIsValid, msg := VerifyPassword(*user.Password, *founduser.Password)
+		defer cancel()
+
+		if !PasswordIsValid{
+			c.JSON{http.StatusInternalServerError, gin.H{"error": msg}}
+			fmt.Println(msg)
+			return 
+		}
+
+		//if password is valid generate token for user
+		token , refreshToken := generate.TokenGenerator(*founduser.Email, *founduser.First_Name, *founduser.Last_Name, *founduser.User_ID)
+		defer cancel()
+
+		generate.UpdateAllTokens(token, refreshToken, founduser.User_ID)
+
+		//response body
+		c.JSON(http.StatusFound, founduser)
+
+	}
+
+} 
+
+
+
+func ProductViewerAdmin() gin.HandlerFunc{
+
+} 
+
+
+
+func SearchProduct() gin.HandlerFunc{
+	return func(c *gin.Context){
+
+		var productlist []models.Product 
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		cursor, err := ProductCollection.Find(ctx, bson.D{{}})
+		if err!= nil{
+			c.IndentedJSON(http.StatusInternalServerError, "something went wrong, please try after sometime")
+			return 
+		}
+
+		cursor.All(ctx, &productlist)
+
+		if err!= nil{
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return 
+		}
+
+		defer cursor.Close()
+
+		if err := cursor.Close(); err != nil{
+			log.Println(err)
+			c.IndentedJSON(400, "invalid")
+			return 
+		}
+
+		defer cancel() 
+		c.IndentedJSON(200, productlist )
+	}
+
+}
+
+
+
+func SearchProductByQuery() gin.HandlerFunc{
+	return func(c *gin.Context){
+		var searchProducts []models.Product 
+		queryParam := c.Query("name")
+
+		//check if query if empty or not
+		if queryParam == ""{
+			log.Println("query is empty")
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"Error":"Invalid search index"})
+			c.Abort()
+			return 
+		}
+
+		//set context
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+
+		// if query has name, find it in database and capture it in a variable  (Find() → gives cursor) Cursor: A pointer to results, not actual data yet
+		searchquerydb, err := ProductCollection.Find(ctx, bson.M{"product_name": bson.M{"$regex": queryParam}})
+		if err!= nil{
+			c.IndentedJSON(404, "something went wrong while fetching the data")
+			return 
+		}
+
+		//Read all data from cursor and convert into Go structs; All() → converts cursor → slice
+		err = searchquerydb.All(ctx, &searchProducts)
+		if err!= nil{
+			log.Println(err)
+			c.IndentedJSON(400, "invalid")
+			return 
+		}
+
+		defer searchquerydb.Close(ctx)
+
+		if err := searchquerydb.Err(); err!= nil{
+			log.Println(err)
+			c.IndentedJSON(400, "invalid request")
+			return 
+		}
+
+		defer cancel()
+		c.IndentedJSON(200, searchProducts)
+
+	}
+
+}
+
+
+
+
+
