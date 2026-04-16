ec398e958523b8c0ae4c0121b2e17b45befcdd33
Mounika <mounikavari9@gmail.com.com>
2026-04-16 13:09:14 -0500

    feat: implement get cart items and total price handler

--- controllers/cart.go
+++ controllers/cart.go
@@ -121,6 +121,58 @@ func (app *Application) RemoveItem() gin.HandlerFunc{
 
 
 func GetItemFromCart() gin.HandlerFunc{
+	return func(c *gin.Context){
+
+		//get id from request
+		user_id := c.Query("id")
+
+		//check if user_id is empty
+		if user_id == ""{
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"error":"invalid id"})
+			c.Abort()
+			return 
+		}
+
+		usert_id, _ := primitive.ObjectIDFromHex(user_id)
+
+		//create context for database operations
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		//create a variable to get item from cart
+		var filledcart models.User
+		err := UserCollection.FindOne(ctx, bson.D{primitive.E{Key:"_id", Value: usert_id}}).Decode(&filledcart)
+
+		if err!= nil{
+			log.Println(err)
+			c.IndentedJSON(500, "not found")
+			return 
+		}
+
+		//create a variable
+		filter_match := bson.D{{Key:"$match", Value: bson.D{primitive.E{Key:"_id", Value:usert_id}}}}
+
+		unwind := bson.D{{Key:"$unwind", Value: bson.D{primitive.E{Key:"path", Value:"$usercart"}}}}
+
+		grouping := bson.D{{Key:"$group", Value: bson.D{primitive.E{Key:"p", Value:"$_id"}, {Key:"total", Value: bson.D{primitive.E{Key:"$sum", Value: "$usercart.price"}} }}}}
+
+		pointcursor, err := UserCollection.Aggregate(ctx, mongo.Pipeline{filter_match, unwind, grouping})
+		if err!= nil{
+			log.Println(err)
+		}
+		var listing []bson.M
+		if err = pointcursor.All(ctx, &listing); err!= nil{
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+		}
+
+		for _, json := range listing{
+			c.IndentedJSON(200, json["total"])
+			c.IndentedJSON(200, filledcart.UserCart)
+		}
+		ctx.Done()
+	}
 
 }
 
