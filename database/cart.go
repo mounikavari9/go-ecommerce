17b3c2d3b91c81d70e82fbbb446128f9163dcc0d
Mounika <mounikavari9@gmail.com.com>
2026-06-11 19:20:51 -0500

    feat: implement add product to cart database operation

--- database/cart.go
+++ database/cart.go
@@ -1,1 +1,60 @@
 package database
+
+import (
+	"github.com/mounikavari9/go-ecommerce/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"context"
+	"log"
+
+)
+
+var (
+
+	ErrCantFindProduct = errors.New("can't find the product")
+	ErrCantDecodeProducts = errors.New("can't find the product")
+	ErrUserIdIsNotValid = errors.New("this user is not valid")
+	ErrCantUpdateUser = errors.New("can't add this product to the cart")
+	ErrCantRemoveItemCart = errors.New("can't remove this item from the cart")
+	ErrCantItem = errors.New("was unable to get the item from the cart")
+	ErrCantBuyCartItem = errors.New("can't update the purchase")
+
+)
+
+func AddProductToCart(ctx context.Context, prodCollection, userCollection *mongo.Collection, productID primitive.ObjectID, userID string) error {
+	searchfromdb, err := prodCollection.Find(ctx, bson.M{"_id": productID})
+	if err != nil{
+		log.Println(err)
+		return ErrCantFindProduct
+	}
+	var productCart []models.ProductUser
+	err = searchfromdb.All(ctx, &productCart)
+	if err!= nil{
+		log.Println(err)
+		return ErrCantDecodeProducts
+	}
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err!= nil{
+		log.Println(err)
+		return ErrUserIdIsNotValid
+	}
+
+	filter := bson.D{primitive.E{Key:"_id", Value: id}}
+	update := bson.D{{Key:"$push", Value: bson.D{primitive.E{key:"usercart", Value: bson.D{{Key:"$each", Value:productCart}}}}}}
+	_, err := userCollection.UpdateOne(ctx, filter, update)
+	if err!= nil{
+		return ErrCantUpdateUser
+	}
+	return nil 
+}
+
+func RemoveCartItem(){
+
+}
+
+func BuyItemFromCart(){
+
+}
+
+func InstantBuyer(){
+
+}
